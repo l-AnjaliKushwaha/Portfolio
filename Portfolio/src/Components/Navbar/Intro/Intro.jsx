a04080e479a49042fe5e88bfb7a54f8d0a681bf1
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-06-25 16:31:42 +0530

    "Updated Intro component CSS and JSX, and Navbar component JSX"

--- Portfolio/src/Components/Navbar/Intro/Intro.jsx
+++ Portfolio/src/Components/Navbar/Intro/Intro.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Intro.css";
-import Github from '../../../img/github.png';
-import LinkedIn from '../../../img/linkedin.png';
-import Instagram from '../../../img/instagram.png';
-import Vector1 from '../../../img/Vector1.png';
+import Github from "../../../img/github.png";
+import LinkedIn from "../../../img/linkedin.png";
+import Instagram from "../../../img/instagram.png";
+import Vector1 from "../../../img/Vector1.png";
 import Vector2 from "../../../img/Vector2.png";
 import girl from "../../../img/girl.png";
-import thumbup from '../../../img/thumbup.png';
-import Crown from '../../../img/crown.png';
+import thumbup from "../../../img/thumbup.png";
+import Crown from "../../../img/crown.png";
 import glassesimoji from "../../../img/glassesimoji.png";
 import FloatingDiv from "../FloatingDiv/FloatingDiv.jsx";
 
@@ -19,7 +19,7 @@ function Intro() {
           <span>Hey! I am </span>
           <span>Anjali Kushwaha</span>
           <span>
-            a MERN Stack developer. Passionate about creating responsive web
+            A MERN Stack developer. Passionate about creating responsive web
             applications using modern JavaScript technologies.
           </span>
         </div>
@@ -41,12 +41,15 @@ function Intro() {
         <img src={Vector2} alt="" />
         <img src={girl} alt="" />
         <img src={glassesimoji} alt="" />
-        <div style={{top: '-6%', left: '80%'}}>
-          <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+        <div style={{ top: "-6%", left: "80%" }}>
+          <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
         </div>
-        <div style={{top: '19rem', left: '0rem'}}>
-          <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award'/>
+        <div style={{ top: "19rem", left: "-2rem" }}>
+          <FloatingDiv image={thumbup} txt1="Best Design" txt2="Award" />
         </div>
+        {/* blur divs */}
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div className="blur" style={{ background: '#C1F5FF', top: '17rem', width: '21rem', height: '11rem', left: '-9rem'}}></div>
       </div>
     </div>
   );

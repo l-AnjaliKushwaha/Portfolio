3ec146db5e8221e127c1cd15d111e3be4a8b77c7
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-06-25 14:38:54 +0530

    "Updated Intro component CSS and JSX files to add styles and social media icons"

--- Portfolio/src/Components/Navbar/Intro/Intro.jsx
+++ Portfolio/src/Components/Navbar/Intro/Intro.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Intro.css";
+import Github from '../../../img/github.png';
+import LinkedIn from '../../../img/linkedin.png';
+import Instagram from '../../../img/instagram.png';
 
 function Intro() {
   return (
@@ -14,10 +17,19 @@ function Intro() {
           </span>
         </div>
         <button className=" button i-button">Hire me</button>
+        <div className="i-icons">
+          <a href="https://github.com/l-AnjaliKushwaha">
+            <img src={Github} alt="Github-Icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/anjali-kushwaha-515561251/">
+            <img src={LinkedIn} alt="LinkedIn-Icon" />
+          </a>
+          <a href="">
+            <img src={Instagram} alt="Instagram-Icon" />
+          </a>
+        </div>
       </div>
-      <div className="i-right">
-            I am right side
-      </div>
+      <div className="i-right">I am right side</div>
     </div>
   );
 }

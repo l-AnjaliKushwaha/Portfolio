e5ff0313c2efe8cdfb72e1042894b434b2442211
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-06-26 23:25:38 +0530

    "Updated Project and Skills components with new CSS styles and Swiper library configuration; added new project images"

--- Portfolio/src/Components/Projects/Project.jsx
+++ Portfolio/src/Components/Projects/Project.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import './Project.css';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import Project1 from '../../img/project1.png';
+import React from "react";
+import "./Project.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import { Navigation, Pagination } from "swiper/modules";
+
+import Project1 from "../../img/project1.png";
 import Project2 from "../../img/project2.png";
 import Project3 from "../../img/project3.png";
 import Project4 from "../../img/project4.png";
 import Project5 from "../../img/project5.png";
 import Project6 from "../../img/project6.png";
 import Project7 from "../../img/project7.png";
-
-
-
-import "swiper/css";
+import Project8 from "../../img/project8.png";
+import Project9 from "../../img/project9.png";
 
 function Project() {
   return (
@@ -21,37 +22,53 @@ function Project() {
       <span>Portfolio</span>
 
       {/* slider */}
-      <Swiper>
+      <Swiper
+        // spaceBetween={30}
+        slidesPerView={3}
+        // grabCursor={true}
+        className="project-slider"
+        modules={[Navigation, Pagination]}
+        navigation={{ enabled: true }}
+        // pagination={{ clickable: true }}
+      >
         <SwiperSlide>
-          <img src={Project1} alt="" />
+          <img src={Project1} alt="Project 1" />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Project2} alt="" />
+          <img src={Project2} alt="Project 2" />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Project3} alt="" />
+          <img src={Project3} alt="Project 3" />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Project4} alt="" />
+          <img src={Project4} alt="Project 4" />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Project5} alt="" />
+          <img src={Project5} alt="Project 5" />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Project6} alt="" />
+          <img src={Project6} alt="Project 6" />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src={Project7} alt="" />
+          <img src={Project7} alt="Project 6" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src={Project8} alt="Project 8" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src={Project9} alt="Project 9" />
         </SwiperSlide>
       </Swiper>
     </div>
   );
 }
 
-export default Project
+export default Project;
